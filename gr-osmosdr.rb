efe750f9466ef2019d7c4e26fc785ae99ee1dcf3
Phil Behnke <phil@pbehnke.com>
2015-03-22 17:28:39 -0400

    Dont use the 3.6 branch of gr-osmosdr

--- gr-osmosdr.rb
+++ gr-osmosdr.rb
@@ -2,8 +2,7 @@ require 'formula'
 
 class GrOsmosdr < Formula
   homepage 'http://sdr.osmocom.org/trac/wiki/GrOsmoSDR'
-  head 'git://git.osmocom.org/gr-osmosdr', :branch => 'gr3.6',
-    :shallow => false
+  head 'git://git.osmocom.org/gr-osmosdr', :shallow => false
 
   depends_on 'cmake' => :build
   depends_on 'gnuradio'
